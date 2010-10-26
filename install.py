8135a0843da20ebe50de34a869e7c4885c4c5a03
Nicola Paolucci <durden@gmail.com>
2010-10-26 17:26:29 +0200

    Initial install.py tracking configs

---
+++ install.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import os
+import glob
+from os.path import join,normpath,abspath
+from pprint import pprint
+import shell
+
+cfgname = '.cfg'
+bkpname = 'backup.cfg'
+gitrepo = 'git@github.com:durdn/cfg.git'
+
+home = normpath(os.environ['HOME'])
+backup_folder = normpath(join(home,bkpname)) 
+cfg_folder = normpath(join(home,cfgname))
+sh = shell.Shell()
+
+if not os.path.exists(backup_folder):
+    os.mkdir(backup_folder)
+
+def list_all(path):
+    "lists all files in a folder, including dotfiles"
+    tmp = sum([glob.glob(normpath(join(a[0],'*')))  for a in os.walk(path)],[])
+    return tmp + sum([glob.glob(normpath(join(a[0],'.*')))  for a in os.walk(path)],[])
+
+def assets(rootpath,flist,test):
+    return filter(lambda x: test(x), [normpath(join(rootpath,f)) for f in flist])
+
+def local_assets(flist,test):
+    return assets(home,flist,test)
+
+def cfg_assets(flist,test):
+    return filter(lambda x: test(x) and os.path.split(x)[1] not in ['.git','.gitignore'], 
+                  assets(cfg_folder,flist,test))
+
+def backup_affected_assets(cfg_folder, backup_folder):
+    "Backup files that would be overwritten by config tracking into backup folder"
+    files = local_assets(os.listdir(cfg_folder),lambda x: True)
+    for f in files:
+        if os.path.exists(f):
+            name = os.path.split(f)[1]
+            print 'mv %s %s' % (f,join(backup_folder,name))
+
+def install_tracked_assets(cfg_folder, destination_folder):
+    "Install tracked configuration files into destination folder"
+    files = cfg_assets(os.listdir(cfg_folder),os.path.isfile)
+    for f in files:
+        name = os.path.split(f)[1]
+        print 'ln %s %s' % (f,join(destination_folder,name))
+    dirs = cfg_assets(os.listdir(cfg_folder),os.path.isdir)
+    for d in dirs:
+        name = os.path.split(d)[1]
+        print 'ln -s %s %s' % (d,join(destination_folder,name))
+
+
+if __name__ == '__main__':
+    print 'cfg 0.1'
+    print 'home=',home
+    print 'backup_folder=',backup_folder
+    print 'cfg_folder=',cfg_folder
+
+    if not os.path.exists(cfg_folder):
+        #clone cfg repo
+        sh.call("git clone %s %s" % (gitrepo,cfgfolder))
+    else:
+        if os.path.exists(join(cfg_folder,'.git')):
+            print 'cfg already cloned to',cfg_folder
+        else:
+            print 'git tracking projects not found in %s' % cfg_folder
+    #print 'local assets'
+    #pprint(local_assets(os.listdir(cfg_folder),os.path.isfile))
+    #pprint(local_assets(os.listdir(cfg_folder),os.path.isdir))
+    #print 'cfg assets'
+    #pprint(cfg_assets(os.listdir(cfg_folder),os.path.isfile))
+    #pprint(cfg_assets(os.listdir(cfg_folder),os.path.isdir))
+    stats = cfg_assets(os.listdir(cfg_folder),lambda x: True)
+    print '* tracking %d assets in %s' % (len(stats),cfgname)
+    print '* backing up affected files...'
+    backup_affected_assets(cfg_folder,backup_folder)
+    print '* installing tracked config files...'
+    install_tracked_assets(cfg_folder,home)
