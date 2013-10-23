9a7e0cc2e570dfc2e9a9c0402f516beff14be59c
Nicola Paolucci <nick@durdn.com>
2013-10-23 11:20:23 +0200

    [install] not git archive extraction fix

--- install.sh
+++ install.sh
@@ -126,8 +126,8 @@ if [ ! -e $cfg_folder ];
         echo "|* git not available downloading zip file..."
         curl -LsO https://github.com/durdn/cfg/archive/master.tar.gz
         tar zxvf master.tar.gz
-        mv master $home/.cfg
-        rm master.tar.gz
+        mv cfg-master $home/.cfg
+        rm cfg-master.tar.gz
     else
       #git is available, clone from repo
       echo "|-> git clone from repo $gitrepo"
