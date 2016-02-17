a8bc5724031655cd7d38a6a23ff50a54ecf03887
Nicola Paolucci <npaolucci@atlassian.com>
2016-02-17 11:22:33 +0100

    Install script for bare repo tracking

---
+++ .bin/install.sh
@@ -0,0 +1,14 @@
+git clone --bare https://bitbucket.org/durdn/cfg.git $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
